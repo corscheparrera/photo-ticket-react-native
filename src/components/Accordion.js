f573dcea1e766e3e9c731956349128db157f1fd6
Maxime Lussier <m.lussier@live.ca>
2018-07-18 15:06:50 -0400

    collapse sections of infractions infos

---
+++ src/components/Accordion.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Animated,
+  ScrollView
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+
+class Accordion extends Component {
+  constructor(props) {
+    super(props);
+
+    this.icons = {
+      up: "ios-arrow-up",
+      down: "ios-arrow-down"
+    };
+
+    this.state = {
+      title: props.title,
+      expanded: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  };
+
+  render() {
+    let icon = this.icons["down"];
+
+    if (this.state.expanded) {
+      icon = this.icons["up"];
+    }
+
+    return (
+      <View style={{ marginBottom: 15 }}>
+        <TouchableHighlight onPress={this.toggle} underlayColor="#f1f1f1">
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>{this.state.title}</Text>
+            <View style={styles.button}>
+              <Icon name={icon} size={28} />
+            </View>
+          </View>
+        </TouchableHighlight>
+        <View style={{ display: this.state.expanded ? "flex" : "none" }}>
+          <ScrollView styles={{ flex: 0 }}>
+            <View style={styles.content}>{this.props.children}</View>
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    margin: 10,
+    overflow: "hidden"
+  },
+  titleContainer: {
+    backgroundColor: "#BE1551",
+    flexDirection: "row"
+  },
+  content: {
+    padding: 15
+  },
+  title: {
+    flex: 1,
+    padding: 10,
+    color: "#FFFFFF",
+    fontWeight: "bold"
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 30
+  },
+  body: {
+    padding: 10,
+    paddingTop: 0
+  }
+});
+
+export default Accordion;
--- src/containers/InfractionView.js
+++ src/containers/InfractionView.js
@@ -13,11 +13,12 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import firebase from "react-native-firebase";
 import Header from "../components/Header";
 import BackendChat from "../utils/BackendChat";
+// import Accordion from "react-native-collapsible/Accordion";
+import Accordion from "../components/Accordion";
 
 const storage = firebase.storage();
-const database = firebase.database();
 
-export default class Cam extends React.Component {
+export default class InfractionView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -52,6 +53,7 @@ export default class Cam extends React.Component {
       data: this.props.data
     });
   };
+
   renderLoadingView = () => {
     if (this.state.isLoading) {
       return (
@@ -66,41 +68,46 @@ export default class Cam extends React.Component {
     let { navigation, data } = this.props;
     if (!this.state.isLoading) {
       return (
-        <View>
+        <View style={styles.container}>
           <Header title="Infraction" navigation={navigation} />
           <ScrollView>
-            <View style={styles.content}>
-              <View style={styles.paragraph}>
-                <Text style={styles.title}>{data.art}</Text>
-                <Text style={styles.title}>{data.source}</Text>
-              </View>
-              <View style={styles.paragraph}>
-                <Text style={styles.title}>Résumé:</Text>
-                <Text style={styles.text}>{data.infraction}</Text>
-              </View>
-              <View style={styles.paragraph}>
-                <Text style={styles.title}>Conditions:</Text>
-                <Text style={styles.text}>{data.conditions}</Text>
-                <View style={styles.saveOrDiscard}>
-                  <Icon
-                    style={styles.icon}
-                    name="save"
-                    size={34}
-                    onPress={() => this.saveData(this.props.imagePath)}
-                  />
-                  <Icon
-                    style={styles.icon}
-                    name="trash-o"
-                    size={34}
-                    onPress={() => {}}
-                  />
-                </View>
-                <Button
-                  title="Consulter un avocat gratuitement"
-                  onPress={() => navigation.navigate("ChatContainer")}
-                />
-              </View>
+            <Text style={styles.headerText}>
+              {data.art + " du " + data.source}
+            </Text>
+
+            <Accordion title="Infraction">
+              <Text style={styles.text}>{data.infraction}</Text>
+            </Accordion>
+            <Accordion title="Conditions">
+              <Text style={styles.text}>{data.conditions}</Text>
+            </Accordion>
+            <Accordion title="Exceptions">
+              <Text style={styles.text}>{data.exceptions}</Text>
+            </Accordion>
+            <Accordion title="Points">
+              <Text style={styles.text}>{data.point}</Text>
+            </Accordion>
+            <Accordion title="Texte de loi">
+              <Text style={styles.text}>{data.loi}</Text>
+            </Accordion>
+            <View style={styles.saveOrDiscard}>
+              <Icon
+                style={styles.icon}
+                name="save"
+                size={34}
+                onPress={() => this.saveData(this.props.imagePath)}
+              />
+              <Icon
+                style={styles.icon}
+                name="trash-o"
+                size={34}
+                onPress={() => {}}
+              />
             </View>
+            <Button
+              title="Consulter un avocat gratuitement"
+              onPress={() => navigation.navigate("ChatContainer")}
+            />
           </ScrollView>
         </View>
       );
@@ -121,7 +128,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#F5FCFF"
   },
   paragraph: {
-    paddingBottom: 25
+    paddingBottom: 25,
+    marginBottom: 35
   },
   content: {
     flex: 1,
@@ -135,11 +143,12 @@ const styles = StyleSheet.create({
     // ...systemWeights.light,
     fontSize: 14
   },
-  title: {
+  headerText: {
     ...material.titleObject,
     color: iOSColors.black,
     ...systemWeights.semibold,
-    fontSize: 16
+    fontSize: 16,
+    textAlign: "center"
   },
   icon: {
     color: "#BE1551"
@@ -150,9 +159,55 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around"
   },
+  header: {
+    backgroundColor: "#BE1551",
+    padding: 10
+  },
   loader: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
   }
 });
+
+{
+  /* // <View>
+        //   <Header title="Infraction" navigation={navigation} />
+        //   <ScrollView>
+        //     <View style={styles.content}>
+        //       <View style={styles.paragraph}>
+        //         <Text style={styles.title}>
+        //           {data.art + " du " + data.source}
+        //         </Text>
+        //         {/* <Text style={styles.title}>{data.source}</Text> */
+}
+//       </View>
+//       <View style={styles.paragraph}>
+//         <Text style={styles.title}>Résumé:</Text>
+//         <Text style={styles.text}>{data.infraction}</Text>
+//       </View>
+//       <View style={styles.paragraph}>
+//         <Text style={styles.title}>Conditions:</Text>
+//         <Text style={styles.text}>{data.conditions}</Text>
+//         <View style={styles.saveOrDiscard}>
+//           <Icon */}
+//             style={styles.icon}
+//             name="save"
+//             size={34}
+//             onPress={() => this.saveData(this.props.imagePath)}
+//           />
+//           <Icon
+//             style={styles.icon}
+//             name="trash-o"
+//             size={34}
+//             onPress={() => {}}
+//           />
+//         </View>
+//         <Button
+//           title="Consulter un avocat gratuitement"
+//           onPress={() => navigation.navigate("ChatContainer")}
+//         />
+//       </View>
+//     </View>
+//   </ScrollView>
+// </View>
