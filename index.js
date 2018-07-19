074def3c5e473480173ba0ee75587c0f1fa4120e
Maxime Lussier <m.lussier@live.ca>
2018-07-19 14:45:26 -0400

    ngninx server + encryption

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { AppRegistry } from "react-native";
 import App from "./App";
-GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest;
 
 // Disable React Native yellow box warnings
 console.disableYellowBox = true;
